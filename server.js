8c7163aeb14ab898719189ead17056d9a1b707c0
Jemboy <dcem@sabanciuniv.edu>
2015-11-02 23:07:26 +0200

    Simplifying the server.. no more SocketIO

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 var http = require("http");	
 
 const PORT = 8080;
-x = 50.0;
-y = 99.5;
 
-var server = http.createServer(function (request, response) {
+var userIDs = ["Jemboy"];
+
+var server = http.createServer(function(request, response) {
 	response.writeHead(200, "Header", {"Content-Type": "application/json"});
 	console.log("Connected.");
-	var jsonObject = {"latitude" : 40.891662, "longitude" : 29.378559};
-	response.end(JSON.stringify(jsonObject));
+
+	request.on("data", function(data) {
+		var request = post["request"];
+		if (request == "upload") {
+			console.log("I am in UPLOAD().");
+			var localID = post["localID"];
+			if (userIDs.hasOwnProperty(localID)) {
+				console.log("Fail");
+				response.end("Fail");
+			}
+			else {
+				console.log("Success");
+				userIDs.push(localID);
+				response.end("Success");
+			}
+		}
+	});
+
+
+
+
+
+	
 }).listen(PORT, function () {
 	console.log("Server listening at: http://localhost:" + PORT);
 });
+
+/*
+var jsonString = '[]';
+var jsonArray = JSON.parse(jsonString);
+jsonArray.push({"userId" : request["userId"], "latitude" : request["latitude"], "longitude" : request["longitude"]});
+var jsonObject = {"latitude" : 40.891662, "longitude" : 29.378559};
+response.end(JSON.stringify(jsonObject));
+*/
