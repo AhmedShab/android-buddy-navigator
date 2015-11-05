9165c12e7b9e94baab6caa0de19ebe1914a458ef
Jemboy <dcem@sabanciuniv.edu>
2015-11-05 15:27:25 +0200

    Class that sends a request to the server to delete a certain ID
    A check is made to block users from deleting other people's IDs.

---
+++ src/Network/DeleteIDTask.java
@@ -0,0 +1,37 @@
+package jemboy.navitwo.Network;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DeleteIDTask extends AsyncTask<String, Void, Void> {
+    String serverIP;
+
+    public DeleteIDTask(String serverIP) {
+        this.serverIP = serverIP;
+    }
+
+    protected Void doInBackground(String... params) {
+        try {
+            URL url = new URL(serverIP);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            String requestID = "request=delete&targetID=" + params[0];
+            connection.setFixedLengthStreamingMode(requestID.getBytes().length);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+            writer.write(requestID);
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
