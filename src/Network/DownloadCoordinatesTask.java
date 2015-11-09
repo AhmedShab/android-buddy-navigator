f7cd21867e62728c198f011d5020da2b86317af4
Jemboy <dcem@sabanciuniv.edu>
2015-11-10 01:21:02 +0200

    Setting up a class to download coordinates

---
+++ src/Network/DownloadCoordinatesTask.java
@@ -0,0 +1,19 @@
+package jemboy.navitwo.Network;
+
+import android.os.AsyncTask;
+
+public class DownloadCoordinatesTask extends AsyncTask<Void, Void, Void> {
+
+    public DownloadCoordinatesTask() {
+
+    }
+
+    protected Void doInBackground(Void... params) {
+
+        return null;
+    }
+
+    protected void onPostExecute(Void param) {
+
+    }
+}
