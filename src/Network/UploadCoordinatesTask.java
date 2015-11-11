7ca6a7ebb65edfb9d8e2c58d55bae23275c3eee4
Jemboy <dcem@sabanciuniv.edu>
2015-11-11 20:05:59 +0200

    Setting up a class to upload coordinates

---
+++ src/Network/UploadCoordinatesTask.java
@@ -0,0 +1,19 @@
+package jemboy.navitwo.Network;
+
+import android.os.AsyncTask;
+
+public class UploadCoordinatesTask extends AsyncTask<Void, Void, Void> {
+
+    public UploadCoordinatesTask() {
+
+    }
+
+    protected Void doInBackground(Void... params) {
+
+        return null;
+    }
+
+    protected void onPostExecute(Void param) {
+
+    }
+}
