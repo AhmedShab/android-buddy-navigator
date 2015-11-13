b85c758bb04a0c6d588a8219d269441d4944c8e8
Jemboy <dcem@sabanciuniv.edu>
2015-11-13 16:54:30 +0200

    Added a method to check if a given service is running

--- src/Main/MainActivity.java
+++ src/Main/MainActivity.java
@@ -1,6 +1,8 @@
 package jemboy.navitwo.Main;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
@@ -124,6 +126,16 @@ public class MainActivity extends Activity implements OnTaskCompleted {
         }
         downloadID.setEnabled(true);
         isNetworkBusy = false;
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
